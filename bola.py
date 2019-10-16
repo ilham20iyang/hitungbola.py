ff2f078a9c0fc7d37556cf05c24fb777135fb4a4
ilham20iyang <55141965+ilham20iyang@users.noreply.github.com>
2019-10-16 02:47:36 -0700

    bola.py
    tugas pemrograman lanjut tentang modul dan paket

---
+++ bola.py
@@ -0,0 +1,16 @@
+import math
+
+def luasbola (r):
+	return 4 * math.pi * r**2 
+
+def volumeBola (r):
+	return 4/3 * math.pi * r**3
+
+def biodata(nama,nim,kelas,fakultas):
+	print("nama = ",nama)
+	print("nim = ",nim)
+	print("kelas = ",kelas)
+	print ("fakultas = ",fakultas)
+
+	return biodata
+	
